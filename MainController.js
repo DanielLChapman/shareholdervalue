6787337b07bb53c2f5d4b19563a11ee1ecca8fe7
RustyRadio <danielc@my.ccsu.edu>
2017-02-11 10:42:02 -0500

    YQL slight change

--- MainController.js
+++ MainController.js
@@ -314,7 +314,7 @@ app.controller('MainController', ['$scope', function($scope) {
     intermediate += encodeURIComponent('"') + $scope.stockArray[$scope.stockArray.length-1][0]+ encodeURIComponent('"');
     
     //putting it together for current
-    $scope.currentURL += intermediate + ")&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback=";
+    $scope.currentURL += intermediate + ")&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
     
     //Historic 
     $scope.historicURL = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20IN%20(" + intermediate + ")%20and%20startDate%20%3D%20%22"+$scope.sDate[2]+"-"+$scope.sDate[0]+"-"+$scope.sDate[1]+"%22%20and%20endDate%20%3D%20%22"+$scope.sDate[2]+"-"+$scope.sDate[0]+"-"+$scope.sDate[1]+"%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
